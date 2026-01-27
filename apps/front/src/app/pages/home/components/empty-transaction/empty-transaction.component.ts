0101600b77ffc52fc1c92ae9dc4c4a6aa5950a11
Yannick Lacaute <yannicklacaute@bitcoinismoney.app>
2026-01-27 22:40:24 +0100

    refactor: move home specific components inside page/home directory, some style adjustments

---
+++ apps/front/src/app/pages/home/components/empty-transaction/empty-transaction.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-empty-transaction',
+  standalone: true,
+  templateUrl: './empty-transaction.component.html',
+  styleUrl: './empty-transaction.component.scss',
+})
+export class EmptyTransactionComponent {}
