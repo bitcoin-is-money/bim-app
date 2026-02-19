4f439d35ae0ff0fbe62e41c71b83f4acf8c679ae
Yannick Lacaute <yannicklacaute@bitcoinismoney.app>
2026-02-19 12:10:52 +0100

    feat(front): display app version (commit hash) in about page

--- apps/front/src/app/pages/about/about.page.ts
+++ apps/front/src/app/pages/about/about.page.ts
@@ -1,13 +1,17 @@
 import {Component} from '@angular/core';
 import {TranslateModule} from '@ngx-translate/core';
 import {GoBackHeaderComponent} from '../../components/go-back-header/go-back-header.component';
+import {InfoFieldComponent} from '../../components/info-field/info-field.component';
 import {FullPageLayoutComponent} from '../../layout';
+import {APP_VERSION} from '../../../environments/version';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [TranslateModule, GoBackHeaderComponent, FullPageLayoutComponent],
+  imports: [TranslateModule, GoBackHeaderComponent, InfoFieldComponent, FullPageLayoutComponent],
   templateUrl: './about.page.html',
   styleUrl: './about.page.scss',
 })
-export class AboutPage {}
+export class AboutPage {
+  readonly version = APP_VERSION;
+}
