e96d398fbd97f88b37630d130b5fa55ffaf1cb2d
ylacaute <yannick-externe.lacaute@datanumia.com>
2026-02-22 09:38:57 +0100

    feat(front): add checkbox field component (not used yet)

---
+++ apps/front/src/app/components/checkbox-field/checkbox-field.component.ts
@@ -0,0 +1,17 @@
+import {Component, input, model} from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox-field',
+  standalone: true,
+  templateUrl: './checkbox-field.component.html',
+  styleUrl: './checkbox-field.component.scss',
+})
+export class CheckboxFieldComponent {
+  readonly checked = model(false);
+  readonly label = input('');
+
+  onToggle(event: Event): void {
+    const value = (event.target as HTMLInputElement).checked;
+    this.checked.set(value);
+  }
+}
