c43eb107b607d8c22d763cf32b05d6f7922a87b3
Yannick Lacaute <yannicklacaute@bitcoinismoney.app>
2026-04-12 21:57:16 +0200

    feat(front): gate security deposit refund notification behind env flag (deactivated in prod)

--- apps/front/src/environments/environment.testnet.ts
+++ apps/front/src/environments/environment.testnet.ts
@@ -3,4 +3,5 @@ export const environment = {
   useMockBackend: false,
   useQRCodeScanner: true,
   wbtcTokenAddress: '0x00452bd5c0512a61df7c7be8cfea5e4f893cb40e126bdc40aee6054db955129e', // wbtc sepolia
+  showSecurityDepositRefundNotification: true,
 };
